27f331cf62c2c0bd6a33f9b728d619cb20fbda0b
Yifan Gao <git@gaoyifan.com>
2016-01-11 01:11:55 +0800

    add APPLY_TEMPLATES_ENABLE and APPLY_ATTRIBUTE_FIX_ENABLE

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,26 +26,30 @@ case ${1} in
     *)
         [[ -f $DEFAULT_ENV ]] && source $DEFAULT_ENV
 
-        cd $TEMPLATES_DIR
-        find . -mindepth 1 -type d | while read dir; do mkdir -p ${dir#*.} ; done
-        TEMPLATE_VARIABLES=$(find . -type f -exec grep -P -o '(?<={{).+?(?=}})' {} \; | xargs -n 1 echo | sort | uniq)
-        find . -type f | 
-        while read file; do
-            file_dst=${file#*.}
-            if [[ -f $MD5_CHECKLIST ]]; then
-                cat $MD5_CHECKLIST | grep $file_dst | md5sum -c --quiet > /dev/null 2>&1 && cp $file $file_dst ;
-                [[ ! -f $file_dst ]] && cp $file $file_dst ;
-            else
-                cp $file $file_dst ;
-            fi
-            [[ -n $TEMPLATE_VARIABLES ]] && echo $TEMPLATE_VARIABLES | xargs -n 1 echo | 
-            while read variable; do
-                [[ -n $variable ]] && eval sed -i "s/{{$variable}}/\${$variable}/g" $file_dst ;
+        if [[ -d $TEMPLATES_DIR ]] && [[ $APPLY_TEMPLATES_ENABLE != false ]]; then
+            cd $TEMPLATES_DIR
+            find . -mindepth 1 -type d | while read dir; do mkdir -p ${dir#*.} ; done
+            TEMPLATE_VARIABLES=$(find . -type f -exec grep -P -o '(?<={{).+?(?=}})' {} \; | xargs -n 1 echo | sort | uniq)
+            find . -type f | 
+            while read file; do
+                file_dst=${file#*.}
+                if [[ -f $MD5_CHECKLIST ]]; then
+                    cat $MD5_CHECKLIST | grep $file_dst | md5sum -c --quiet > /dev/null 2>&1 && cp $file $file_dst ;
+                    [[ ! -f $file_dst ]] && cp $file $file_dst ;
+                else
+                    cp $file $file_dst ;
+                fi
+                [[ -n $TEMPLATE_VARIABLES ]] && echo $TEMPLATE_VARIABLES | xargs -n 1 echo | 
+                while read variable; do
+                    [[ -n $variable ]] && eval sed -i "s/{{$variable}}/\${$variable}/g" $file_dst ;
+                done
             done
-        done
+        fi
 
-        [[ -f $ATTRIBUTE_FIX_LIST ]] && cat $ATTRIBUTE_FIX_LIST | awk '{ printf("chmod %s %s\n",$1,$4); }' | sh
-        [[ -f $ATTRIBUTE_FIX_LIST ]] && cat $ATTRIBUTE_FIX_LIST | awk '{ printf("chown %s:%s %s\n",$2,$3,$4); }' | sh
+        if [[ -f $ATTRIBUTE_FIX_LIST ]] && [[ $APPLY_ATTRIBUTE_FIX_ENABLE != false ]]; then
+            cat $ATTRIBUTE_FIX_LIST | awk '{ printf("chmod %s %s\n",$1,$4); }' | sh
+            cat $ATTRIBUTE_FIX_LIST | awk '{ printf("chown %s:%s %s\n",$2,$3,$4); }' | sh
+        fi
 
         exec "$@"
         ;;
