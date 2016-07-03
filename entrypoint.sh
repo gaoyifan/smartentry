7d74806983ce41ce9e639410af9b9b63a60d84f8
Yifan Gao <git@gaoyifan.com>
2016-07-04 01:02:26 +0800

    multiple changes
    * volumes data init
    * add log

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,14 @@ CHMOD_FILE=${CHMOD_FILE:-"$ASSETS_DIR/chmod.list"}
 BUILD_SCRIPT=${BUILD_SCRIPT:-"$ASSETS_DIR/build.sh"}
 VOLUME_INIT_LIST=${VOLUME_INIT_LIST:-"$ASSETS_DIR/volume_init.list"}
 PRE_RUN_SCRIPT=${PRERUN_SCRIPT:="$ASSETS_DIR/pre_run.sh"}
+VOLUMES_LIST=${VOLUMES_LIST:="$ASSETS_DIR/volumes.list"}
+VOLUMES_ARCHIVE=${VOLUMES_ARCHIVE:="$ASSETS_DIR/volumes.tar"}
 
 case ${1} in
     build)
         # run user defined script
         if [[ -f $BUILD_SCRIPT ]]; then
+        echo '>> running build.sh'
             source $BUILD_SCRIPT
         fi
 
@@ -24,6 +27,7 @@ case ${1} in
 
         # generate MD5 list for target files of rootfs, to keep user's modification when docker restart 
         if [[ -d $ROOTFS_DIR ]] && [[ $KEEP_USER_MODIFICATION_ENABLE != false ]] ; then
+        echo '>> generate MD5 list'
             cd $ROOTFS_DIR
             TEMPLATE_VARIABLES=$(find . -type f -exec grep -P -o '(?<={{).+?(?=}})' {} \; | xargs -n 1 echo | sort | uniq ) 
             find . -type f | 
@@ -35,6 +39,7 @@ case ${1} in
 
         # chmod for rootfs
         if [[ -d $ROOTFS_DIR ]] && [[ $CHMOD_AUTO_FIX_ENABLE != false ]]; then
+            echo '>> chmod for rootfs'
             cd $ROOTFS_DIR 
             find . -type f -o -type d |
             while read file; do
@@ -44,13 +49,32 @@ case ${1} in
             [[ -f ${CHMOD_FILE} ]] && cat ${CHMOD_FILE} >> ${CHMOD_FILE}.add
             mv ${CHMOD_FILE}.add ${CHMOD_FILE}
         fi
+
+        # init volume data
+        if [[ -f $VOLUMES_LIST ]] && [[ $INIT_VOLUMES_DATA_ENABLE != false ]]; then
+            echo '>> save volume data'
+            cat $VOLUMES_LIST | xargs tar -rf $VOLUMES_ARCHIVE
+        fi
+
         ;;
+
     *)
         # set default environment variable
-        [[ -f $DEFAULT_ENV_FILE ]] && source $DEFAULT_ENV_FILE
+        if [[ -f $DEFAULT_ENV_FILE ]] ; then
+            echo '>> apply default environment variable'
+            source $DEFAULT_ENV_FILE
+        fi
+
+        # init volume data
+        if [[ -f $VOLUMES_ARCHIVE ]] && [[ $INIT_VOLUMES_DATA_ENABLE != false ]]; then
+            echo '>> init volume data'
+            tar -C / -xf $VOLUMES_ARCHIVE
+            rm $VOLUMES_ARCHIVE
+        fi
 
         # patch file; apply template
         if [[ -d $ROOTFS_DIR ]] && [[ $ROOTFS_ENABLE != false ]]; then
+            echo '>> patch template files'
             cd $ROOTFS_DIR
             find . -mindepth 1 -type d | while read dir; do mkdir -p ${dir#*.} ; done
             TEMPLATE_VARIABLES=$(find . -type f -exec grep -P -o '(?<={{).+?(?=}})' {} \; | xargs -n 1 echo | sort | uniq)
@@ -79,16 +103,19 @@ case ${1} in
 
         # fix file mode
         if [[ -f $CHMOD_FILE ]] && [[ $CHMOD_FIX_ENABLE != false ]]; then
+            echo '>> fix file mode'
             cat $CHMOD_FILE | awk '{ printf("chmod %s %s\n",$1,$4); }' | sh
             cat $CHMOD_FILE | awk '{ printf("chown %s:%s %s\n",$2,$3,$4); }' | sh
         fi
 
         # pre-running script
         if [[ -f $PRE_RUN_SCRIPT ]]; then
+            echo '>> pre-running script'
             source $PRE_RUN_SCRIPT
         fi
 
         # main program
+        echo '>> running main program'
         cd $PWD_ORGI
         exec "$@"
         ;;
