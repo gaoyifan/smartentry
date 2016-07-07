22210dc68375054affe88e7d408f08de91bb1581
Yifan Gao <git@gaoyifan.com>
2016-07-08 01:51:48 +0800

    use variable prompt

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,13 @@ PRE_RUN_SCRIPT=${PRERUN_SCRIPT:="$ASSETS_DIR/pre_run.sh"}
 VOLUMES_LIST=${VOLUMES_LIST:="$ASSETS_DIR/volumes.list"}
 VOLUMES_ARCHIVE=${VOLUMES_ARCHIVE:="$ASSETS_DIR/volumes.tar"}
 
+ENTRY_PROMPT=${ENTRY_PROMPT:="entrypoint>> "}
+
 case ${1} in
     build)
         # run user defined script
         if [[ -f $BUILD_SCRIPT ]]; then
-        echo '>> running build.sh'
+        echo "$ENTRY_PROMPT running build.sh"
             source $BUILD_SCRIPT
         fi
 
@@ -26,7 +28,7 @@ case ${1} in
 
         # generate MD5 list for target files of rootfs, to keep user's modification when docker restart 
         if [[ -d $ROOTFS_DIR ]] && [[ $KEEP_USER_MODIFICATION_ENABLE != false ]] ; then
-        echo '>> generate MD5 list'
+        echo "$ENTRY_PROMPT generate MD5 list"
             cd $ROOTFS_DIR
             TEMPLATE_VARIABLES=$(find . -type f -exec grep -P -o '(?<={{).+?(?=}})' {} \; | xargs -n 1 echo | sort | uniq ) 
             find . -type f | 
@@ -38,7 +40,7 @@ case ${1} in
 
         # chmod for rootfs
         if [[ -d $ROOTFS_DIR ]] && [[ $CHMOD_AUTO_FIX_ENABLE != false ]]; then
-            echo '>> chmod for rootfs'
+            echo "$ENTRY_PROMPT chmod for rootfs"
             cd $ROOTFS_DIR 
             find . -type f -o -type d |
             while read file; do
@@ -51,7 +53,7 @@ case ${1} in
 
         # init volume data
         if [[ -f $VOLUMES_LIST ]]; then
-            echo '>> save volume data'
+            echo "$ENTRY_PROMPT save volume data"
             cat $VOLUMES_LIST | xargs tar -rf $VOLUMES_ARCHIVE
         fi
 
@@ -60,20 +62,20 @@ case ${1} in
     *)
         # set default environment variable
         if [[ -f $DEFAULT_ENV_FILE ]] ; then
-            echo '>> apply default environment variable'
+            echo "$ENTRY_PROMPT apply default environment variable"
             source $DEFAULT_ENV_FILE
         fi
 
         # init volume data
         if [[ -f $VOLUMES_ARCHIVE ]] && [[ $INIT_VOLUMES_DATA_ENABLE == true ]]; then
-            echo '>> init volume data'
+            echo "$ENTRY_PROMPT init volume data"
             tar -C / -xf $VOLUMES_ARCHIVE
             rm $VOLUMES_ARCHIVE
         fi
 
         # patch file; apply template
         if [[ -d $ROOTFS_DIR ]] && [[ $ROOTFS_ENABLE != false ]]; then
-            echo '>> patch template files'
+            echo "$ENTRY_PROMPT patch template files"
             cd $ROOTFS_DIR
             find . -mindepth 1 -type d | while read dir; do mkdir -p ${dir#*.} ; done
             TEMPLATE_VARIABLES=$(find . -type f -exec grep -P -o '(?<={{).+?(?=}})' {} \; | xargs -n 1 echo | sort | uniq)
@@ -102,19 +104,19 @@ case ${1} in
 
         # fix file mode
         if [[ -f $CHMOD_FILE ]] && [[ $CHMOD_FIX_ENABLE != false ]]; then
-            echo '>> fix file mode'
+            echo "$ENTRY_PROMPT fix file mode"
             cat $CHMOD_FILE | awk '{ printf("chmod %s %s\n",$1,$4); }' | sh
             cat $CHMOD_FILE | awk '{ printf("chown %s:%s %s\n",$2,$3,$4); }' | sh
         fi
 
         # pre-running script
         if [[ -f $PRE_RUN_SCRIPT ]]; then
-            echo '>> pre-running script'
+            echo "$ENTRY_PROMPT pre-running script"
             source $PRE_RUN_SCRIPT
         fi
 
         # main program
-        echo '>> running main program'
+        echo "$ENTRY_PROMPT running main program"
         cd $PWD_ORGI
         exec "$@"
         ;;
