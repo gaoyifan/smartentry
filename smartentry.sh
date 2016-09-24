320277407bf2208456d70cc2f4c24cc4ff08f8a5
Yifan Gao <git@yfgao.com>
2016-09-24 19:00:26 +0800

    template name rule: [A-Za-z0-9_]+

--- smartentry.sh
+++ smartentry.sh
@@ -192,7 +192,7 @@ case ${1} in
                 mkdir -p ${dir#*.} ; 
             done
 
-            TEMPLATE_VARIABLES=$(find . -type f -exec grep -o '{{.\+}}' {} \; | awk -vRS='}}' '{gsub(/.*\{\{/,"");print}' | xargs -n 1 echo | sort | uniq)
+            TEMPLATE_VARIABLES=$(find . -type f -exec grep -o '{{[A-Za-z0-9_]\+}}' {} \; | awk -vRS='}}' '{gsub(/.*\{\{/,"");print}' | xargs -n 1 echo | sort | uniq)
             find . -type f | 
             while read file; do
                 file_dst=${file#*.}
